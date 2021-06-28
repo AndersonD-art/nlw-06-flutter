3407ce100ba9cb7714b4ad29a2bc687d9c66e27e
Anderson <andersondavidti@hotmail.com>
2021-06-28 15:57:06 -0300

    ending class 4

--- android/app/src/main/kotlin/br/com/andersond/payflow/MainActivity.kt
+++ android/app/src/main/kotlin/br/com/andersond/payflow/MainActivity.kt
@@ -2,5 +2,5 @@ package br.com.andersond.payflow
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterActivity() {   
 }
--- lib/modules/barcode_scanner/barcode_scanner_page.dart
+++ lib/modules/barcode_scanner/barcode_scanner_page.dart
@@ -19,6 +19,11 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
   @override
   void initState() {
     controller.getAvailableCameras();
+    controller.statusNotifier.addListener(() {
+      if (controller.status.hasBarcode) {
+        Navigator.pushReplacementNamed(context, "/insert_boleto");
+      }
+    });
     super.initState();
   }
 
@@ -30,14 +35,6 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
 
   @override
   Widget build(BuildContext context) {
-    /*  return BottomSheetWidget(
-      title: "Não foi possivel identificar um código de barras.",
-      subtitle: "Tente escanear novamente ou digite o código do seu boleto.",
-      primaryLabel: "Escanear novamente",
-      primaryOnPressed: () {},
-      secondaryLabel: "Digitar código",
-      secondaryOnPressed: () {},
-    ); */
     return SafeArea(
       top: true,
       bottom: true,
@@ -99,6 +96,26 @@ class _BarcodeScannerPageState extends State<BarcodeScannerPage> {
                 secondaryOnPressed: () {},
               ),
             ),
+          ),
+          ValueListenableBuilder<BarcodeScannerStatus>(
+            valueListenable: controller.statusNotifier,
+            builder: (_, status, __) {
+              if (status.hasErros) {
+                return BottomSheetWidget(
+                  title: "Não foi possivel identificar um código de barras.",
+                  subtitle:
+                      "Tente escanear novamente ou digite o código do seu boleto.",
+                  primaryLabel: "Escanear novamente",
+                  primaryOnPressed: () {
+                    controller.getAvailableCameras();
+                  },
+                  secondaryLabel: "Digitar código",
+                  secondaryOnPressed: () {},
+                );
+              } else {
+                return Container();
+              }
+            },
           ),
         ],
       ),
